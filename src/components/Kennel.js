519bdfd25c87709ca73c8c54f2118454e7198964
Zach McWhirter <zmac7811@gmail.com>
2020-07-08 22:37:17 -0500

    got nav bar working

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,47 +1,16 @@
 import React from "react";
+import NavBar from "./nav/NavBar";
+import ApplicationViews from "./ApplicationViews";
+
 import "./Kennel.css";
-import AnimalCard from "./animal/AnimalCard";
-import EmployeeCard from "./employee/EmployeeCard";
-import LocationCard from "./location/LocationCard";
-import OwnerCard from "./owner/OwnerCard";
-
-
-
-
 
 const Kennel = () => {
-    return (
-        <div>
-            <div>
-                <h2>
-                    Students Kennels
-                <br />
-                    <small>Loving care when you're not there.</small>
-                </h2>
-
-                <address>
-                    Visit Us at the Nashville North Location
-            <br />
-            500 Puppy Way
-            </address>
-            </div>
-            <div className="container-cards">
-                <AnimalCard/>
-                <AnimalCard/>
-                <AnimalCard/>
-            </div>
-            <div>
-                <LocationCard/>
-            </div>
-            <div>
-                <OwnerCard/>
-            </div>
-            <div>
-                <EmployeeCard/>
-                <EmployeeCard/>
-            </div>
-        </div>
-    );
+  return (
+    <>
+      <NavBar />
+      <ApplicationViews />
+    </>
+  );
 };
 
 export default Kennel;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import Kennel from './components/Kennel';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <Kennel/>
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
